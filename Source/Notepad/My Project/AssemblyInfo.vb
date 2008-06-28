d5cac8ddf7ee780c234b7043adc5ec984961b42b
linhhonglecao <linhhonglecao@68bf131f-974f-0410-a7b1-49782ce8d8ae>
2008-06-28 09:23:29 +0000

    Chinh sua lai cau truc luu tru tren google.code

---
+++ Source/Notepad/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Notepad")> 
+<Assembly: AssemblyDescription("Đồ án môn Quản Lý Cấu Hình Phần Mềm")> 
+<Assembly: AssemblyCompany("KLMNT")> 
+<Assembly: AssemblyProduct("Notepad")> 
+<Assembly: AssemblyCopyright("Copyright © KLMNT  2008")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a38840c7-94c8-45a8-bb92-f0b036bd090a")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ Source/Notepad/frmAbout.vb
@@ -0,0 +1,175 @@
+﻿Imports System.Net.Mail
+Public NotInheritable Class frmAbout
+
+    Private Sub AboutBox1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ' title
+        Dim ApplicationTitle As String
+        If My.Application.Info.Title <> "" Then
+            ApplicationTitle = My.Application.Info.Title
+        Else
+            ApplicationTitle = System.IO.Path.GetFileNameWithoutExtension(My.Application.Info.AssemblyName)
+        End If
+
+        Me.Text = Space(10) & "KLMNT Notepad."
+
+        TextBox2.Text = ""
+        Me.Button1.Width = 0
+        Me.Button1.Height = 0
+        Me.CancelButton = Button1
+        nCount = 1
+        nRatio = 1
+
+        Me.LinkLabel1.Links(0).LinkData = Me.LinkLabel1.Text
+        Me.LinkLabel2.Links(0).LinkData = Me.LinkLabel2.Text
+        Me.LinkLabel3.Links(0).LinkData = Me.LinkLabel3.Text
+        Me.LinkLabel4.Links(0).LinkData = Me.LinkLabel4.Text
+        Me.LinkLabel5.Links(0).LinkData = Me.LinkLabel5.Text
+        Me.LinkLabel6.Links(0).LinkData = Me.LinkLabel6.Text
+
+    End Sub
+
+    Private nCount As Integer = 1
+    Private nRatio As Integer = 1
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Me.Text = Strings.Right(Me.Text, Me.Text.Length - 1)
+        If Me.Text = "" Then
+            Me.Text = Space(100) & "KLMNT Notepad."
+        End If
+
+        Dim sSpace = "     "
+        Dim sValue As String = ""
+        With TextBox2
+            Select Case nCount
+                Case 1
+                    sValue = " Trường Đại học khoa hoc tự nhiên"
+                    If nRatio = 1 Then
+                        .Text = sValue
+                    Else
+                        .Text = Replace(.Text, sValue, "")
+                    End If
+                Case 2
+                    sValue = " Khoa Công nghệ thông tin"
+                    If nRatio = 1 Then
+                        .Text += vbCrLf.ToString & sValue
+                    Else
+                        .Text = Replace(.Text, sValue, "")
+                    End If
+                Case 3
+                    sValue = " Lớp 06HCB. "
+                    If nRatio = 1 Then
+                        .Text += vbCrLf.ToString & sValue
+                    Else
+                        .Text = Replace(.Text, sValue, "")
+                    End If
+                Case 4
+                    sValue = " ------------------------------------- "
+                    If nRatio = 1 Then
+                        .Text += vbCrLf.ToString & sValue
+                    Else
+                        .Text = Replace(.Text, sValue, "")
+                    End If
+
+                Case 5
+                    sValue = " Đồ án môn Quản lý cấu hình phần mềm "
+                    If nRatio = 1 Then
+                        .Text += vbCrLf.ToString & vbCrLf.ToString & sValue
+                    Else
+                        .Text = Replace(.Text, sValue, "")
+                    End If
+
+                Case 6
+                    sValue = " K L M N T  -  N O T E P A D "
+
+                    If nRatio = 1 Then
+                        .Text += vbCrLf.ToString & vbCrLf.ToString & sValue
+                    Else
+                        .Text = Replace(.Text, sValue, "")
+                    End If
+
+                Case 7
+                    sValue = " Version " & Application.ProductVersion
+                    If nRatio = 1 Then
+                        .Text += vbCrLf.ToString & sValue
+                    Else
+                        .Text = Replace(.Text, sValue, "")
+                    End If
+                Case 8
+                    sValue = " -------------------------------- "
+                    If nRatio = 1 Then
+                        .Text += vbCrLf.ToString & vbCrLf.ToString & sValue
+                    Else
+                        .Text = Replace(.Text, sValue, "")
+                    End If
+
+                Case 9
+                    sValue = " < PHỤ TRÁCH SẢN PHẨM >"
+                    If nRatio = 1 Then
+                        .Text += vbCrLf.ToString & vbCrLf.ToString & sValue
+                    Else
+                        .Text = Replace(.Text, sValue, "")
+                    End If
+
+                Case 10
+                    sValue = " - Cao Lê Hồng Lĩnh. "
+                    If nRatio = 1 Then
+                        .Text += vbCrLf.ToString & vbCrLf.ToString & sValue
+                    Else
+                        .Text = Replace(.Text, sValue, "")
+                    End If
+                Case 11
+                    sValue = " -  Đoàn Thị Huỳnh Mai. "
+                    If nRatio = 1 Then
+                        .Text += vbCrLf.ToString & sValue
+                    Else
+                        .Text = Replace(.Text, sValue, "")
+                    End If
+                Case 12
+                    sValue = " - Nguyễn Thị Kim Trúc. "
+                    If nRatio = 1 Then
+                        .Text += vbCrLf.ToString & sValue
+                    Else
+                        .Text = Replace(.Text, sValue, "")
+                    End If
+                Case 13
+                    sValue = " - Lê Thị Ngà. "
+                    If nRatio = 1 Then
+                        .Text += vbCrLf.ToString & sValue
+                    Else
+                        .Text = Replace(.Text, sValue, "")
+                    End If
+                Case 14
+                    sValue = " - Đồng Nhật Kha. "
+                    If nRatio = 1 Then
+                        .Text += vbCrLf.ToString & sValue
+                    Else
+                        .Text = Replace(.Text, sValue, "")
+                    End If
+                Case Else
+                    nCount = 0
+                    nRatio = (nRatio Mod 2) + 1
+            End Select
+            nCount += 1
+        End With
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+
+
+    Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Dim target As String = CType(e.Link.LinkData, String)
+        If (Nothing <> target) Then
+            System.Diagnostics.Process.Start(target)
+        End If
+
+    End Sub
+    Private Sub LinkLabel6_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel6.LinkClicked, LinkLabel4.LinkClicked, LinkLabel5.LinkClicked, LinkLabel2.LinkClicked, LinkLabel3.LinkClicked
+        Dim target As String = CType(e.Link.LinkData, String)
+        System.Diagnostics.Process.Start("mailto:" & target) ' ."linhhonglecao@gmail.com")
+    End Sub
+
+End Class
